f251321fc64bcf6d02f96f66c340c936f60f388e
Vincent Lin <linv@mit.edu>
2025-03-11 10:36:47 -0400

    complete pset 3.2

--- pset3/src/p2_calibrate_camera.py
+++ pset3/src/p2_calibrate_camera.py
@@ -7,6 +7,7 @@ import numpy as np
 from PIL import Image
 import cv2
 import matplotlib.pyplot as plt
+from scipy.ndimage import map_coordinates
 
 
 def get_3D_object_points(chessboard_size: tuple) -> np.ndarray:
@@ -17,12 +18,16 @@ def get_3D_object_points(chessboard_size: tuple) -> np.ndarray:
     Returns:
         Numpy array containing the 3D object points
     """
-    # TODO: Implement this method!
-    raise NotImplementedError
+    num_cols, num_rows = chessboard_size
+    points = np.array([
+        [col, row, 0] for row in range(num_rows) for col in range(num_cols)
+    ], dtype=np.float32)
+
+    return points
 
 
-def undistort_image(image: np.ndarray, 
-                    camera_matrix: np.ndarray, 
+def undistort_image(image: np.ndarray,
+                    camera_matrix: np.ndarray,
                     dist_coeffs: np.ndarray) -> np.ndarray:
     """
     Undistort an image
@@ -33,17 +38,44 @@ def undistort_image(image: np.ndarray,
     Returns:
         Numpy array containing the undistorted image
     """
-    # TODO: Implement this method!
-    raise NotImplementedError
+    height, width, channels = image.shape
+    inv_camera_matrix = np.linalg.inv(camera_matrix)
+    k1, k2, p1, p2, k3 = dist_coeffs.flatten()
 
+    map_x = np.zeros((height, width), dtype=np.float32)
+    map_y = np.zeros((height, width), dtype=np.float32)
+
+    for v in range(height):
+        for u in range(width):
+            uv_homogenous = np.array([u, v, 1], dtype=np.float32)
+            x, y, _ = inv_camera_matrix @ uv_homogenous # world coordinates
+
+            r = np.sqrt(x**2 + y**2)
+            x_distorted = (x * (1 + k1*r**2 + k2*r**4 + k3*r**6)
+                           + (2*p1*x*y) + p2 * (r**2 + 2*x**2))
+            y_distorted = (y * (1 + k1*r**2 + k2*r**4 + k3*r**6)
+                           + (2*p2*x*y) + p1 * (r**2 + 2*y**2))
+
+            u_distorted, v_distorted, _ = camera_matrix @ np.array([x_distorted, y_distorted, 1]) # pixel coordinates
+            map_x[v, u] = u_distorted
+            map_y[v, u] = v_distorted
+
+    coords = np.stack((map_y.flatten(), map_x.flatten()), axis=0)
+
+    # apply map to each channel individually
+    undistorted_image = np.zeros_like(image)
+    for c in range(channels):
+        mapped_coords = map_coordinates(image[..., c], coords, order=1, mode="nearest").reshape((height, width))
+        undistorted_image[..., c] = mapped_coords.reshape((height, width))
+    return undistorted_image
 
 def load_grayscale_image(image: np.ndarray) -> np.ndarray:
     gray_image = np.mean(image, axis=2).astype(np.uint8)
     return gray_image
 
 
-def calibrate_camera(object_points: np.ndarray, 
-                     corners: np.ndarray, 
+def calibrate_camera(object_points: np.ndarray,
+                     corners: np.ndarray,
                      image_size: tuple) -> tuple:
     ret, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.calibrateCamera(
         [object_points], [corners], image_size, None, None
@@ -57,7 +89,7 @@ def find_chessboard_corners(image: np.ndarray, chessboard_size: tuple) -> np.nda
 
     if ret is False:
         raise ValueError("Verify correct dimensions of chessboard")
-    
+
     return corners
 
 
@@ -77,19 +109,19 @@ def draw_corners(image: np.ndarray, chessboard_size: tuple, corners: np.ndarray)
 
 if __name__ == "__main__":
     if not os.path.exists(env.p2.output):
-        os.makedirs(env.p2.output)  
+        os.makedirs(env.p2.output)
     expected_camera_matrix = np.load(env.p2.expected_camera_matrix)
     expected_dist_coeffs = np.load(env.p2.expected_dist_coeffs)
     # Part 2.a
     ideal_intrinsic_matrix = np.array([
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
+        [237.9237668337108, 0, 0],
+        [0, 237.9237668337108, 0],
+        [0, 0, 1]
     ])
 
     # Part 2.b
     chessboard_size = (14, 9)  # (columns, rows)
-    
+
     image = utils.load_image(env.p1.chessboard_path)
     grayscale_image = load_grayscale_image(image)
     corners = find_chessboard_corners(grayscale_image, chessboard_size)
